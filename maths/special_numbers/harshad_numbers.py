b93b7fea82fbb61fa80c2dfb59ba7884e2c90bda
MaximSmolskiy <mithridatus@mail.ru>
2024-12-24 18:21:30 +0300

    Fix sphinx/build_docs warnings for maths/special_numbers/harshad_numbers

--- maths/special_numbers/harshad_numbers.py
+++ maths/special_numbers/harshad_numbers.py
@@ -1,16 +1,18 @@
 """
 A harshad number (or more specifically an n-harshad number) is a number that's
 divisible by the sum of its digits in some given base n.
+
 Reference: https://en.wikipedia.org/wiki/Harshad_number
 """
 
 
 def int_to_base(number: int, base: int) -> str:
     """
-    Convert a given positive decimal integer to base 'base'.
-    Where 'base' ranges from 2 to 36.
+    Convert a given positive decimal integer `number` to base `base`.
+    Where `base` ranges from ``2`` to ``36``.
 
     Examples:
+
     >>> int_to_base(23, 2)
     '10111'
     >>> int_to_base(58, 5)
@@ -49,11 +51,12 @@ def int_to_base(number: int, base: int) -> str:
 
 def sum_of_digits(num: int, base: int) -> str:
     """
-    Calculate the sum of digit values in a positive integer
-    converted to the given 'base'.
-    Where 'base' ranges from 2 to 36.
+    Calculate the sum of digit values in a positive integer `num`
+    converted to the given `base`.
+    Where `base` ranges from ``2`` to ``36``.
 
     Examples:
+
     >>> sum_of_digits(103, 12)
     '13'
     >>> sum_of_digits(1275, 4)
@@ -82,10 +85,11 @@ def sum_of_digits(num: int, base: int) -> str:
 
 def harshad_numbers_in_base(limit: int, base: int) -> list[str]:
     """
-    Finds all Harshad numbers smaller than num in base 'base'.
-    Where 'base' ranges from 2 to 36.
+    Finds all Harshad numbers smaller than `limit` in base `base`.
+    Where `base` ranges from ``2`` to ``36``.
 
     Examples:
+
     >>> harshad_numbers_in_base(15, 2)
     ['1', '10', '100', '110', '1000', '1010', '1100']
     >>> harshad_numbers_in_base(12, 34)
@@ -120,10 +124,11 @@ def harshad_numbers_in_base(limit: int, base: int) -> list[str]:
 
 def is_harshad_number_in_base(num: int, base: int) -> bool:
     """
-    Determines whether n in base 'base' is a harshad number.
-    Where 'base' ranges from 2 to 36.
+    Determines whether `num` in base `base` is a harshad number.
+    Where `base` ranges from ``2`` to ``36``.
 
     Examples:
+
     >>> is_harshad_number_in_base(18, 10)
     True
     >>> is_harshad_number_in_base(21, 10)
